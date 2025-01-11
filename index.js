876f6642613ce2aa47ff027b6716d6cd98162831
rikki <funkybutknowledged@gmail.com>
2025-01-11 15:35:36 -0500

    afihaoile

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ function appendToDisplay(input) {
     display.value += input;
 }
 
-function clearDisplayt() {
-    display.value = "";
+function clearDisplay() {
+   display.value = " ";
 }
 
 function calculate() {
-    
+    try{
+        display.value = eval(display.value)
+    }
+    catch(error){
+        alert("error try again ;)")
+    }
 }
+    
+    
+
+
