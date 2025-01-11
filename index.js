d8e241835c98089fed58ae386d1524f47e340728
rikki <funkybutknowledged@gmail.com>
2025-01-11 11:33:22 -0500

    ,

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const display = document.getElementById("display")
 
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function clearDisplayt() {
+    display.value = "";
+}
+
+function calculate() {
+    
+}
